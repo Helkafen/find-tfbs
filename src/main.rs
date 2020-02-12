d3aaa1ba41da060a3d92f14c7539509c960768f7
Sebastian Meric de Bellefon <sebastian.meric.de.bellefon@umontreal.ca>
2020-02-12 15:39:08 -0500

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -189,126 +189,140 @@ fn main() {
     }
 
     let (merged_peaks, peak_map) = load_peak_files(&bed_files, &chromosome, after_position);
+    let number_of_peaks = merged_peaks.len();
 
-    let (tx, rx): (Sender<String>, Receiver<String>) = mpsc::channel();
-    let (peak_tx, peak_rx) = multiqueue::broadcast_queue(100000);
+    let bcf_samples: Vec<String> = get_sample_names(&IndexedReader::from_path(&bcf).expect("Error while opening the bcf file"));
+    let bcf_samples_len = bcf_samples.len();
 
-    let _writer_thread = thread::spawn(move || {
-        let mut writer = BGzWriter::new(BufWriter::with_capacity(4096*1000,fs::File::create(output_file.clone() + ".part").expect("Could not create output file")));
-        loop {
-            match rx.recv() {
-                Ok(s) => { /*if s.contains("GATA1") {println!("Some GATA1"); };*/ /*println!("Some");*/ writer.write(s.as_bytes()).expect("Could not write bytes to output file"); }
-                Err(_) => { let _ = writer.flush(); break; }
+    let _writer_thread = {
+        // Communication channels
+        let (tx, rx): (Sender<String>, Receiver<String>) = mpsc::channel();
+        let (peak_tx, peak_rx) = multiqueue::broadcast_queue(merged_peaks.len() as u64);
+
+        // The writer thread receive lines of text and writes them to the output file
+        let _writer_thread = thread::spawn(move || {
+            {
+                let mut writer = BGzWriter::new(BufWriter::with_capacity(4096*1000,fs::File::create(output_file.clone() + ".part").expect("Could not create output file")));
+                loop {
+                    match rx.recv() {
+                        Ok(s) => { writer.write(s.as_bytes()).expect("Could not write bytes to output file"); }
+                        Err(_) => { let _ = writer.flush(); break; }
+                    }
+                };
+
+                let _ = writer.flush();
+            }
+            if run_tabix{
+                let x = Exec::shell(&format!("zcat {}.part | bgzip > {}; tabix -f -p vcf {}; rm {}.part", output_file.clone(), output_file.clone(), output_file.clone(), output_file.clone())).join().unwrap();
+                if x.success() {
+                    println!("Tabixed file {}", output_file.clone());
+                }
+                else {
+                    println!("Failed to tabix file {}", output_file.clone());
+                }
+            }
+            else {
+                fs::rename(output_file.clone() + ".part", output_file.clone()).expect(&format!("Count not rename {} into {}", output_file.clone(), output_file.clone() + ".part"));
+            }
+            println!("End of writer thread");
+        });
+
+        // Find the individuals and their column number in the BCF file
+        let (samples, sample_positions_in_bcf): (Vec<String>, Vec<usize>) = {
+            match opt_matches.value_of("samples") {
+            None => (bcf_samples, (0..bcf_samples_len).into_iter().collect()),
+            Some(f) => {
+                    let input = File::open(f).expect(&format!("Could not open sample file {}", f));
+                    let buffered = BufReader::new(input);
+                    let mut samples = Vec::new();
+                    for line in buffered.lines() { if let Ok(l) = line { if l.len() > 1 { samples.push(l); } } }
+                    let mut sample_indices = Vec::new();
+                    let samples_set: HashSet<String> = samples.clone().into_iter().collect();
+                    for i in 0..bcf_samples_len {
+                        if samples_set.contains(&bcf_samples[i]) {
+                            sample_indices.push(i);
+                        }
+                    }
+                    (samples, sample_indices)
+            },
             }
         };
-        if run_tabix{
-            println!("Tabixing {}", output_file.clone());
-            let bgzip_exit_status = Exec::shell(&format!("zcat {}.part | bgzip > {}; tabix -f -p vcf {}; rm {}.part", output_file.clone(), output_file.clone(), output_file.clone(), output_file.clone())).join().unwrap();
-            println!("HERE2 {:#?}", bgzip_exit_status); // TODO: Mysteriously, we never reach this point. It doesn't affect the functionality.
-        }
-        else {
-            fs::rename(output_file.clone() + ".part", output_file.clone()).expect(&format!("Count not rename {} into {}", output_file.clone(), output_file.clone() + ".part"));
-        }
-        let _ = writer.flush();
-        println!("End of writer");
-    });
+        let sample_count = samples.len();
+        let null_count: Vec<u32> = repeat(sample_count, 0);
+        println!("Reading {} samples out of {}", sample_count, bcf_samples_len);
+        let all_haplotypes_with_reference_genome: HashSet<HaplotypeId> = all_haplotype_ids(sample_count);
 
-    let mut reader = IndexedReader::from_path(&bcf).expect("Error while opening the bcf file");
-    let bcf_samples = get_sample_names(&mut reader);
-    let bcf_samples_length = bcf_samples.len();
-    let number_of_peaks: usize = merged_peaks.len();
+        // Write header in output file
+        tx.send("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT".to_string()).expect("Could not create output file");
+        for sample in samples {
+            tx.send("\t".to_string()).expect("Could not create output file");
+            tx.send(sample.clone()).expect("Could not create output file");
+        }tx.send("\n".to_string()).expect("Could not create output file");
 
-    let (samples, sample_positions_in_bcf): (Vec<String>, Vec<usize>) = {
-        match opt_matches.value_of("samples") {
-           None => (bcf_samples, (0..bcf_samples_length).into_iter().collect()),
-           Some(f) => {
-                let input = File::open(f).expect(&format!("Could not open sample file {}", f));
-                let buffered = BufReader::new(input);
-                let mut samples = Vec::new();
-                for line in buffered.lines() { if let Ok(l) = line { if l.len() > 1 { samples.push(l); } } }
-                let mut sample_indices = Vec::new();
-                let samples_set: HashSet<String> = samples.clone().into_iter().collect();
-                for i in 0..bcf_samples_length {
-                    if samples_set.contains(&bcf_samples[i]) {
-                        sample_indices.push(i);
-                    }
+        let start_time = SystemTime::now();
+
+        // A fake and unique position in the chromosome given for each line in the resulting vcf
+        let fake_position: Arc<Mutex<u32>> = Arc::new(Mutex::new(1));
+        let number_of_peaks_processed: Arc<Mutex<u32>> = Arc::new(Mutex::new(0));
+
+        let mut handlers = vec![];
+        for _ in 0..threads {
+            let chromosome                = Arc::new(chromosome.clone());
+            let peak_map                  = Arc::new(peak_map.clone());
+            let pwm_list                  = Arc::new(pwm_list.clone());
+            let number_of_peaks_processed = number_of_peaks_processed.clone();
+            let sample_positions_in_bcf   = Arc::new(sample_positions_in_bcf.clone());
+            let null_count                = Arc::new(null_count.clone());
+            let pwm_name_dict             = Arc::new(pwm_name_dict.clone());
+            let fake_position             = fake_position.clone();
+            let tx                        = tx.clone();
+            let peak_rx                   = peak_rx.clone();
+            let all_haplotypes_with_reference_genome = Arc::new(all_haplotypes_with_reference_genome.clone());
+            let mut reader = rust_htslib::bcf::IndexedReader::from_path(&bcf).expect("Error while opening the bcf file");
+            let mut reference_genome = bio::io::fasta::IndexedReader::from_file(&Path::new(&reference_genome_file)).expect(&format!("Error while opening the reference genome '{}'", reference_genome_file));
+
+            let handle = thread::spawn(move || {
+                for peak in peak_rx {
+                    process_peak(&chromosome,
+                                 &mut reader,
+                                 &mut reference_genome,
+                                 peak,
+                                 &peak_map,
+                                 &pwm_list,
+                                 &sample_positions_in_bcf,
+                                 &null_count,
+                                 &pwm_name_dict,
+                                 mode_0_vs_n,
+                                 min_maf,
+                                 &all_haplotypes_with_reference_genome,
+                                 start_time,
+                                 &tx,
+                                 fake_position.clone(),
+                                 number_of_peaks,
+                                 number_of_peaks_processed.clone());
                 }
-                (samples, sample_indices)
-           },
+            });
+            handlers.push(handle);
         }
-    };
-    let sample_count = samples.len();
-    let null_count: Vec<u32> = repeat(sample_count, 0);
-    println!("Reading {} samples out of {}", sample_count, bcf_samples_length);
-    let all_haplotypes_with_reference_genome: HashSet<HaplotypeId> = all_haplotype_ids(sample_count);
 
-    // Write header in output file
-    tx.send("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT".to_string()).expect("Could not create output file");
-    for sample in samples {
-        tx.send("\t".to_string()).expect("Could not create output file");
-        tx.send(sample.clone()).expect("Could not create output file");
-    }tx.send("\n".to_string()).expect("Could not create output file");
+        for peak in merged_peaks {
+            let _ = peak_tx.try_send(peak).unwrap();
+        }
+        drop(peak_tx); // Let the worker threads know that this is the end of the input stream
 
+        for thread in handlers {
+            let _ = thread.join();
+        }
+        _writer_thread
+    }; // tx dies here, which allows the writer thread to terminate
 
-    let start_time = SystemTime::now();
+    let _ = _writer_thread.join();
 
-    // A fake and unique position in the chromosome given for each line in the resulting vcf
-    let fake_position: Arc<Mutex<u32>> = Arc::new(Mutex::new(1));
-    let number_of_peaks_processed: Arc<Mutex<u32>> = Arc::new(Mutex::new(0));
-
-    let mut handlers = vec![];
-    for _ in 1..threads {
-        let chromosome                = Arc::new(chromosome.clone());
-        let bcf                       = Arc::new(bcf.clone());
-        let reference_genome_file     = Arc::new(reference_genome_file.clone());
-        let peak_map                  = Arc::new(peak_map.clone());
-        let pwm_list                  = Arc::new(pwm_list.clone());
-        let number_of_peaks_processed = number_of_peaks_processed.clone();
-        let sample_positions_in_bcf   = Arc::new(sample_positions_in_bcf.clone());
-        let null_count                = Arc::new(null_count.clone());
-        let pwm_name_dict             = Arc::new(pwm_name_dict.clone());
-        let fake_position             = fake_position.clone();
-        let tx                        = tx.clone();
-        let peak_rx                   = peak_rx.clone();
-        let all_haplotypes_with_reference_genome = Arc::new(all_haplotypes_with_reference_genome.clone());
-
-        let handle = thread::spawn(move || {
-            for peak in peak_rx {
-                process_peak(&chromosome,
-                             &bcf,
-                             &reference_genome_file,
-                             peak,
-                             &peak_map,
-                             &pwm_list,
-                             &sample_positions_in_bcf,
-                             &null_count,
-                             &pwm_name_dict,
-                             mode_0_vs_n,
-                             min_maf,
-                             &all_haplotypes_with_reference_genome,
-                             start_time,
-                             &tx,
-                             fake_position.clone(),
-                             number_of_peaks,
-                             number_of_peaks_processed.clone());
-            }
-        });
-        handlers.push(handle);
-    }
-
-    for peak in merged_peaks {
-        let _ = peak_tx.try_send(peak).unwrap();
-    }
-    drop(peak_tx);
-
-
-    for thread in handlers {
-        let _ = thread.join();
-    }
-
+    println!("End.");
 }
 
-fn process_peak(chromosome: &str, bcf: &str, reference_genome_file: &str, peak: Range, peak_map: &HashMap<String, Vec<range::Range>>,
+
+fn process_peak(chromosome: &str, reader: &mut IndexedReader, reference_genome: &mut bio::io::fasta::IndexedReader<std::fs::File>, peak: Range, peak_map: &HashMap<String, Vec<range::Range>>,
                 pwm_list: &Vec<PWM>, sample_positions_in_bcf: &Vec<usize>,
                 null_count: &Vec<u32>, pwm_name_dict: &HashMap<u16, String>, mode_0_vs_n: bool, min_maf: u32,
                 all_haplotypes_with_reference_genome: &HashSet<HaplotypeId>, start_time: SystemTime, txx: &Sender<String>,
@@ -317,15 +331,11 @@ fn process_peak(chromosome: &str, bcf: &str, reference_genome_file: &str, peak: 
 
     let chr = chromosome.replace("chr", "");
 
-    let mut reader = IndexedReader::from_path(bcf).expect("Error while opening the bcf file");
-
-    let mut reference_genome = bio::io::fasta::IndexedReader::from_file(&Path::new(reference_genome_file.clone())).expect(&format!("Error while opening the reference genome '{}'", reference_genome_file));
-
-    let ref_haplotype = read_peak_in_reference_genome(chromosome.to_string(), &peak, &mut reference_genome);
+    let ref_haplotype = read_peak_in_reference_genome(chromosome.to_string(), &peak, reference_genome);
 
     let inner_peaks: HashMap<&String, Vec<&Range>> = select_inner_peaks(peak, &peak_map);
 
-    let (match_list, number_of_haplotypes, number_of_variants) = find_all_matches(&chromosome, &peak, &mut reader, &ref_haplotype, &*pwm_list, (*all_haplotypes_with_reference_genome).clone(), &*sample_positions_in_bcf);
+    let (match_list, number_of_haplotypes, number_of_variants) = find_all_matches(&chromosome, &peak, reader, &ref_haplotype, &*pwm_list, (*all_haplotypes_with_reference_genome).clone(), &*sample_positions_in_bcf);
 
     for ((source, inner_peak, pattern_id),(v1,v2)) in count_matches_by_sample(&match_list, &inner_peaks, &*null_count).drain() {
         let pwm_name = pwm_name_dict.get(&pattern_id).expect("Logic error: No pattern name for a pattern_id");
